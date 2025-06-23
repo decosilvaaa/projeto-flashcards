5e96b129e4970382555c355ca816e68ea3acea37
decosilvaaa <andre.rohling.silva@escola.pr.gov.br>
2025-06-23 11:47:24 +0000

    alteraçoes

---
+++ arquivosJS/perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Geografia',
+    'Qual é a capital da Noruega?',
+    'Capital é Oslo.'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual o maior campeão do paulistão?',
+    'Corinthians com 31 títulos.'
+)
+
+criaCartao(
+    'futebol',
+    'Qual é o maior campeão das copas do mundo?',
+    'A seleção brasileira.'
+)
+
+criaCartao(
+    'Música',
+    'Em que ano o Michael Jackson morreu?',
+    'Ele morreu em 2009.'
+)
+
+criaCartao(
+    'Música',
+    'Em que ano Kanye West lançou mbdtf?',
+    'Em 2010 após polêmicas.'
+)
+
+criaCartao(
+    'Cinema',
+    'Qual ator interpreta o homem de ferro?',
+    'Robert Downey Jr.'
+)
